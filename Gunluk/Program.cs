387cde2c05734ca5c09180588cd9f87641e82275
Mustafa Oguzcan Kacmaz <oguzcankacmaz@hotmail.com>
2023-01-10 17:03:00 +0300

    baslangicmigration

--- Gunluk/Program.cs
+++ Gunluk/Program.cs
@@ -34,4 +34,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetService<UygulamaDbContext>();
+    db?.Database.Migrate();
+}
 app.Run();
